3a5211694506a9ce9879548d57fa27ea47ad9cfd
Kharmon <Kyleharmon52@gmail.com>
2023-12-17 21:02:52 -0500

    tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(params[:id])
+    @charts = @user.charts
+    # Ensure that the user can only access their own profile
+    redirect_to(root_path) unless @user == current_user
   end
 end
