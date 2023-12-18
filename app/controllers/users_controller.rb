b873a46462dcf82d9e4ff3824dfcf057cab3698b
Kharmon <Kyleharmon52@gmail.com>
2023-12-18 17:49:52 -0500

    Prep for branching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     @charts = @user.charts
     # Ensure that the user can only access their own profile
     redirect_to(root_path) unless @user == current_user
